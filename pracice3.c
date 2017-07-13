48414bbc92c715df0f42a390574872dadcd132aa
Esther.Jeong <estherjeong@EstherJeongs-MacBook-Air.local>
2017-07-13 15:14:22 +0900

    add doc

--- pracice3.c
+++ pracice3.c
@@ -8,8 +8,9 @@
 // for celsius = 0, 20, ...., 300; floating-point version
 int main()
 {
+//celsius 선언하기
 	float celsius;	
-
+//for 문 이용하여 define한 값을 사용하여 함수 만들기 - celsius to fahrenheit
 	for (celsius = LOWER; celsius <= UPPER; celsius = celsius +STEP)
 		printf("%3f %6.2f\n", celsius, (9.0/5.0)/(celsius+32));
 }
